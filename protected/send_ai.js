b370da9c105691aac066d682012b81824fd2aed7
insidebeing7-ux <insidebeing7@gmail.com>
2026-05-15 14:58:40 +0100

    Update send_ai.js

--- protected/send_ai.js
+++ protected/send_ai.js
@@ -18,7 +18,7 @@ window.addEventListener("DOMContentLoaded", () => {
     box.innerText = "Thinking...";
 
     try {
-      const res = await fetch("/ai", {
+      const res =  await fetch("/ai-send", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
